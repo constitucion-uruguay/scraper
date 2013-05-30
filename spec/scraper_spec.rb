c2868ee5ac293301fc09a224d80745b8d80f3da6
gusaaaaa <gusa@neo.com>
2013-05-30 08:03:49 -0300

    Adds more cases to specs: WIP

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'rspec/given'
 require 'scraper'
 
@@ -14,11 +16,11 @@ describe Scraper do
     Scraper.new(html).to_markdown
   end
 
-  context "with no contents" do
+  context "Begin" do
     Given(:input) { [] }
     Then { output.should == "" }
 
-    context "receiving title" do
+    context "> Title" do
       When {
         input << <<-html
           <h2 align="center">CONSTITUCION DE LA REPUBLICA</h2>
@@ -31,7 +33,7 @@ describe Scraper do
         md
       }
 
-      context "receiving subtitle" do
+      context "> Subtitle" do
         When {
           input << <<-html
             <h4 align="center">CONSTITUCION 1967 CON LAS MODIFICACIONES PLEBISCITADAS EL 26 DE
@@ -43,6 +45,382 @@ describe Scraper do
             CONSTITUCION 1967 CON LAS MODIFICACIONES PLEBISCITADAS EL 26 DE NOVIEMBRE DE 1989, EL 26 DE NOVIEMBRE DE 1994 Y EL 8 DE DICIEMBRE DE 1996
           md
         }
+
+        context "> Section" do
+          When {
+            input << <<-html
+              <h4 align="center">SECCION I</h4>
+            html
+          }
+          Then {
+            output.should end_with <<-md.gsub(/^ +/, '')
+              SECCION I
+              ---------
+            md
+          }
+
+          context "> Section subtitle" do
+            When {
+              input << <<-html
+                <h4 align="center">DE LA NACION Y SU SOBERANIA</h4>
+              html
+            }
+            Then {
+              output.should end_with <<-md.gsub(/^ +/, '')
+                DE LA NACION Y SU SOBERANIA
+              md
+            }
+
+            context "> Chapter" do
+              When {
+                input << <<-html
+                  <h4 align="center">CAPITULO I</h4>
+                html
+              }
+              Then {
+                output.should end_with <<-md.gsub(/^ +/, '')
+
+                  ### CAPITULO I
+                md
+              }
+
+              context "> Article" do
+                When {
+                  input << <<-html
+                    <p><u><a name="art1">Artículo 1º</a></u>.- Lorem ipsum</p>
+                  html
+                }
+                Then {
+                  output.should end_with <<-md.gsub(/^ +/, '')
+                    __Artículo 1__. Lorem ipsum
+                  md
+                }
+                context "followed by articles" do
+                  When {
+                    input << <<-html
+                      <p><u><a name="art2">Artículo 2</a></u>.- Lorem ipsum.</p>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+
+                      __Artículo 2__. Lorem ipsum.
+                    md
+                  }
+                end
+
+                context "with aditional paragraphs" do
+                  When {
+                    input << <<-html
+                      <p>Lorem ipsum</p>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+
+                      Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with items in a TABLE" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tr>
+                          <td>A)</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                        <tr>
+                          <td>B)</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+
+                        A. Lorem ipsum
+
+                        B. Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with items in a TABLE with TBODY" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td>A)</td>
+                            <td>Lorem ipsum</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+
+                        A. Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with items enumerated with primes" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tr>
+                          <td>A')</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+
+                        A'. Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with items and subitems" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tr>
+                          <td>A)</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>a)</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md
+
+A. Lorem ipsum
+
+  a. Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with just subitems" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tr>
+                          <td></td>
+                          <td>a)</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md
+
+  a. Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with items spanning many paragraphs" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tr>
+                          <td>A)</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md
+
+A. Lorem ipsum
+
+  Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with items and subitems in rows containing blank columns" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tr>
+                          <td>A)</td>
+                          <td></td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td></td>
+                          <td>a)</td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+
+A. Lorem ipsum
+
+  a. Lorem ipsum
+                    md
+                  }
+                end
+
+                context "with items in rows containing special references" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tr>
+                          <td>A)</td>
+                          <td><a>*</a></td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                        <tr>
+                          <td>B)</td>
+                          <td><a>***</a></td>
+                          <td>Lorem ipsum</td>
+                        </tr>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+
+                        A. Lorem ipsum
+
+                        B. Lorem ipsum
+                    md
+                  }
+                end
+
+              end
+
+              context "> Article with ordinal indicator" do
+                When {
+                  input << <<-html
+                    <p><u><a name="art1">Artículo 1º</a></u>.- Lorem ipsum.</p>
+                  html
+                }
+                Then {
+                  output.should end_with <<-md.gsub(/^ +/, '')
+                    __Artículo 1__. Lorem ipsum.
+                  md
+                }
+              end
+
+              context "> Article with line breaks" do
+                When {
+                  input << <<-html
+                    <p><u><a>Artículo 1</a></u>.- Lorem
+                    ipsum</p>
+                  html
+                }
+                Then {
+                  output.should end_with <<-md.gsub(/^ +/, '')
+                    __Artículo 1__. Lorem ipsum
+                  md
+                }
+              end
+
+              context "> Article with BR tags" do
+                When {
+                  input << <<-html
+                    <p><u><a>Artículo 1</a></u>.- Lorem<br> ipsum</p>
+                  html
+                }
+                Then {
+                  output.should end_with <<-md.gsub(/^ +/, '')
+                    __Artículo 1__. Lorem ipsum
+                  md
+                }
+              end
+
+              context "> Transitional arrangements" do
+                When {
+                  input << <<-html
+                    <h4>DISPOSICIONES TRANSITORIAS</h4>
+                  html
+                }
+                Then {
+                  output.should end_with <<-md.gsub(/^ +/, '')
+
+                    ## DISPOSICIONES TRANSITORIAS
+                  md
+                }
+
+                context "> With items in a TABLE" do
+                  When {
+                    input << <<-html
+                      <table>
+                        <tbody>
+                          <tr>
+                            <td>A)</td>
+                            <td>Lorem ipsum</td>
+                          </tr
+                        </tbody>
+                      </table>
+                    html
+                  }
+                  Then {
+                    output.should end_with <<-md.gsub(/^ +/, '')
+                        A. Lorem ipsum
+                    md
+                  }
+                end
+              end
+            end
+
+            context "> Unique chapter" do
+              When {
+                input << <<-html
+                  <h4>CAPITULO UNICO</h4>
+                html
+              }
+              Then {
+                output.should end_with <<-md.gsub(/^ +/, '')
+                  ### CAPITULO UNICO
+                md
+              }
+            end
+          end
+
+          context "> Section subtitle in multiple lines" do
+            When {
+              input << <<-html
+                <h4>Lorem ipsum</h4>
+                <h4>Lorem ipsum</h4>
+              html
+            }
+            Then {
+              output.should end_with <<-md.gsub(/^ +/, '')
+                Lorem ipsum
+
+                Lorem ipsum
+              md
+            }
+          end
+        end
       end
     end
   end
